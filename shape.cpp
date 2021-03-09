8d90dcf2a29c423a14c0ca0015b99a9a06082945
charlotte gray <charlottetgray@gmail.com>
2021-03-09 16:49:55 -0500

    tried to keep up

--- shape.cpp
+++ shape.cpp
@@ -1,6 +1,17 @@
 #include "shape.h"
 
-Shape::Shape()
-{
+Shape::Shape() { }
+
+Rect::Rect(double _width, double _length):width(_width),length(_length) { }
+double Rect::area() const {return length*width;}
+
+void Rect::setLength(double _length) { length = _length; }
+void Rect::setWidth(double _width) { width = _width; }
+
+Square::Square(double _side){ }
+
+void Square::setLength(double _length) {setSide(_length);}
+void Square::setWidth(double _width) {setSide(_width);}
+void Square::setSide(double _side) {
 
 }
--- shape.h
+++ shape.h
@@ -11,13 +11,19 @@ public:
     //TODO: ask why i can't make one that returns strings
 };
 
+class Rect : public Shape {
+    double length, width;
+public:
+    void setWidth(double _width);
+    void setLength(double _length);
+};
+
 
 class Square: public Shape {
     Square() {
 
     }
-    virtual double area() const override;
-    //this is a good place to have override-- not required, but good to note that we know it overrides a member function!
+    virtual double area() const override;                   //this is a good place to have override-- not required, but good to note that we know it overrides a member function!
     virtual double perimeter() const override;
     virtual int sides() const override;
 };
